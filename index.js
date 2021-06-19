fda75f5cf13057bd25395f662e16684b100cddd2
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-18 19:12:58 -0500

    adding intern and asking for new employee in the prompt for a recursive function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Employee = require('./lib/Employee')
+const Intern = require('./lib/Intern')
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -39,11 +42,45 @@ function role() {
             message: `What is the engineer's Github?`,
             when: (data) => data.role === 'Engineer',
         },
+        {
+            type: 'input',
+            name: 'school',
+            message: `What is the name of the school?`,
+            when: (data) => data.role === 'Intern',
+        },
+        {
+            type: 'confirm',
+            name: 'newEmployee',
+            message: 'Do you have another employee to add?'
+        },
     ])
-    .then((answers) => console.log(new Manager(answers.name, answers.id, answers.email, answers.officeNumber)))
+    
+    .then((answers) => {
+       
+        if(answers.role === 'Manager'){
+            console.log(new Manager(answers.name, answers.id, answers.email, answers.officeNumber)) 
+        }else {
+            if(answers.role === 'Engineer') {
+                console.log(new Engineer(answers.name, answers.id, answers.email, answers.github))
+            } else{
+                if(answers.role === 'Intern'){
+                    console.log(new Intern(answers.name, answers.id, answers.email, answers.school))
+                } else{
+                    console.log(new Employee(answers.name, answers.id, answers.email))
+                    
+                }
+            }
+
+        }
+        if(answers.newEmployee === true){
+            return role();
+         }
+    })
+
     .catch((err) => console.log(err))
     
 }
+
 
 
 
