eca3e7ff216f33c281f62d7a1f9a1e707e7a9ce1
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-20 23:35:10 -0500

    Adding the start of the engineer test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,3 +1,28 @@
 const Engineer = require('../lib/Engineer');
 
-describe('Engineer Class')
+describe('Engineer Class', () => {
+    describe('Create new Engineer class', () => {
+        it('Should create a new object from the Engineer Class', () => {
+            // Act
+            const engineer = new Engineer('Alex', 'C75', 'alexC@gmail.com', 'AlexC');
+            // Assert
+            expect(new Engineer()).toBeInstanceOf(Engineer);
+        });
+    });
+    describe('engineer.name', () => {
+        it("Should return the engineer's name", () => {
+            // Act
+            const engineer = new Engineer('Alex', 'C75', 'alexC@gmail.com', 'AlexC');
+            // Assert
+            expect(engineer.name).toEqual('Alex');
+        });
+    });
+    describe('engineer.id', () => {
+        it("Should return the engineer's name", () => {
+            // Act
+            const engineer = new Engineer('Alex', 'C75', 'alexC@gmail.com', 'AlexC');
+            // Assert
+            expect(engineer.id).toEqual('C75');
+        });
+    });
+});
