dc0ccf1522a6d47ad3233a7b06f85bda277750f1
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-20 22:48:37 -0500

    Adding test for Employee Class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,70 @@
-const employee = require('Employee');
+const Employee = require('../lib/Employee');
 
-describe('', () => {
-    //test
-    //Arrange
-    //Assert
-})
+describe('Employee Class', () => {
+    describe("Create new Employee should be an instanceof Employee class", () => {
+        //Test
+        it("Should create a new Employee object", () => {
+            //Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(new Employee()).toBeInstanceOf(Employee);
+
+        })
+    });
+    describe("employee.name", () => {
+        it("Should return a string of the Employee name", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.name).toEqual('Heather')
+        });
+    });
+    describe("employee.id", () => {
+        it("Should return a string of the Employee ID", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.id).toEqual('B250')
+        });
+    });
+    describe("employee.email", () => {
+        it("Should return a string of the Employee email", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.email).toEqual('h.m.bjoin@gmail.com')
+        });
+    });
+    describe("getname", () => {
+        it("Should return a string of the Employee email", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.getName()).toEqual('Heather')
+        });
+    });
+    describe("getId", () => {
+        it("Should return a string of the Employee email", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.getId()).toEqual('B250')
+        });
+    });
+    describe("getEmail", () => {
+        it("Should return the Employee email", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.getEmail()).toEqual('h.m.bjoin@gmail.com')
+        });
+    });
+    describe("getRole", () => {
+        it("Should return the Employee role", () => {
+            // Act
+            const employee = new Employee('Heather', 'B250', 'h.m.bjoin@gmail.com');
+            //Assert
+            expect(employee.getRole()).toEqual('Employee')
+        });
+    });
+});
---
+++ _tests_/Manager.test.js
@@ -0,0 +1,12 @@
+const Manager = require('../lib/Manager');
+
+describe('Manager Class', () => {
+    describe("Create new Manager should be an instanceof Manager Class", () => {
+        it("Should create a new Manager object", () => {
+            //Act
+            const manager = new Manager('Kelly', 'D22', 'kelly@gmail', 'B24');
+            //Assert
+            
+        });
+    });
+});
