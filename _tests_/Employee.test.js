8c2fb519ef9fc91b7e902e37d35f59fe58d97ea0
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-20 19:41:47 -0500

    Finishing adding all the things to generate the HTML

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,7 @@
+const employee = require('Employee');
+
+describe('', () => {
+    //test
+    //Arrange
+    //Assert
+})
--- index.js
+++ index.js
@@ -1,15 +1,100 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateHTML = require('./src/generateHTML')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Employee = require('./lib/Employee')
 const Intern = require('./lib/Intern')
+const managerArray = [];
+const engineerArray = [];
+const employeeArray = [];
+const internArray = [];
 
 const writeFileAsync = util.promisify(fs.writeFile);
+let html = ``
+async function managerCard(){
+    for(let i = 0; i < managerArray.length; i++){
+    html += `<!-- Manager Card-->
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${managerArray[i].name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+                <p class="card-text">${managerArray[i].id}</p>
+                <p class="card-text">${managerArray[i].officeNumber}</p>
+                <a href="mailto:${managerArray[i].email}" class="card-link">Email</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+
+    `
+    }
+}
+
+async function engineerCard(){
+    for(let i = 0; i < engineerArray.length; i++){
+    html += `<!-- Engineer Card-->
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${engineerArray[i].name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
+                <p class="card-text">${engineerArray[i].id}</p>
+                <p class="card-text">${engineerArray[i].github}</p>
+                <a href="mailto:${engineerArray[i].email}" class="card-link">Email</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+
+    `
+    }
+}
+
+async function employeeCard(){
+    for(let i = 0; i < employeeArray.length; i++){
+    html += `<!-- Engineer Card-->
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${employeeArray[i].name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Employee</h6>
+                <p class="card-text">${employeeArray[i].id}</p>
+                <a href="mailto:${employeeArray[i].email}" class="card-link">Email</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+
+    `
+    }
+}
+
+async function internCard(){
+    for(let i = 0; i < internArray.length; i++){
+    html += `<!-- Engineer Card-->
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${internArray[i].name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Employee</h6>
+                <p class="card-text">${internArray[i].id}</p>
+                <p class="card-text">${internArray[i].school}</p>
+                <a href="mailto:${internArray[i].email}" class="card-link">Email</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+
+    `
+    }
+}
+
 
 function role() {
-   inquirer.prompt([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -23,7 +108,7 @@ function role() {
         {
             type: `input`,
             name: `email`,
-            message:`What is the employee's email?`,
+            message: `What is the employee's email?`,
         },
         {
             type: 'list',
@@ -34,7 +119,7 @@ function role() {
             type: 'input',
             name: 'officeNumber',
             message: `What is the manager's office number?`,
-            when: (data) => data.role === 'Manager', 
+            when: (data) => data.role === 'Manager',
         },
         {
             type: 'input',
@@ -54,31 +139,52 @@ function role() {
             message: 'Do you have another employee to add?'
         },
     ])
-    
-    .then((answers) => {
-       
-        if(answers.role === 'Manager'){
-            console.log(new Manager(answers.name, answers.id, answers.email, answers.officeNumber)) 
-        }else {
-            if(answers.role === 'Engineer') {
-                console.log(new Engineer(answers.name, answers.id, answers.email, answers.github))
-            } else{
-                if(answers.role === 'Intern'){
-                    console.log(new Intern(answers.name, answers.id, answers.email, answers.school))
-                } else{
-                    console.log(new Employee(answers.name, answers.id, answers.email))
-                    
+
+        .then((response) => {
+            let employee;
+            if (response.role === 'Manager') {
+                employee = new Manager(response.name, response.id, response.email, response.officeNumber)
+                managerArray.push(employee)
+                console.log('managerArray', managerArray)
+                managerCard()
+                console.log(new Manager(response.name, response.id, response.email, response.officeNumber))
+            } else {
+                if (response.role === 'Engineer') {
+                    employee = new Engineer(response.name, response.id, response.email, response.github)
+                    engineerArray.push(employee)
+                    engineerCard()
+                    console.log(new Engineer(response.name, response.id, response.email, response.github))
+                } else {
+                    if (response.role === 'Intern') {
+                        employee = new Intern(response.name, response.id, response.email, response.school)
+                        internArray.push(employee)
+                        internCard()
+                        console.log(new Intern(response.name, response.id, response.email, response.school))
+                    }
+                    else {
+                        employee = new Employee(response.name, response.id, response.email)
+                        employeeArray.push(employee)
+                        employeeCard()
+                        console.log(new Employee(response.name, response.id, response.email))
+                    }
                 }
+
             }
+            console.log('managerArray', managerArray)
+            console.log('engineerArray', engineerArray)
+            console.log('internArray', internArray)
+            console.log('employeeArray', employeeArray)
+            if (response.newEmployee === true) {
+                return role();
+            }
+            
+            
+        })
+        
+        .then((response) => writeFileAsync('./dist/index.html', generateHTML(response, html)))
 
-        }
-        if(answers.newEmployee === true){
-            return role();
-         }
-    })
+        .catch((err) => console.log(err))
 
-    .catch((err) => console.log(err))
-    
 }
 
 
